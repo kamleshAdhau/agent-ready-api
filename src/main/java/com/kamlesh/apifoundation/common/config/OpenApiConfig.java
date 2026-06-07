61f8edad7a91f29cd058e2620ef86ebbf26f16af
kamleshAdhau <adhaukamlesh4@gmail.com>
2026-06-07 18:04:12 +0530

    Add OpenAPI configuration

---
+++ src/main/java/com/kamlesh/apifoundation/common/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.kamlesh.apifoundation.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info()
+                .title("Agent Ready API")
+                .version("v1")
+                .description("Spring Boot API foundation for agent-ready applications"));
+    }
+}
