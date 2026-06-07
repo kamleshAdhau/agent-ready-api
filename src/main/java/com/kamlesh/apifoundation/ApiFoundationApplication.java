49dbc364ec18bce8a3e43df1de289e292940d8ee
kamleshAdhau <adhaukamlesh4@gmail.com>
2026-06-07 17:22:05 +0530

    Add Spring Boot application bootstrap

---
+++ src/main/java/com/kamlesh/apifoundation/ApiFoundationApplication.java
@@ -0,0 +1,12 @@
+package com.kamlesh.apifoundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFoundationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFoundationApplication.class, args);
+    }
+}
